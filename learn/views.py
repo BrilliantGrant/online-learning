a95fb1b5ce52e7e7f029104763b78f564b8d1378
bri <briegrant416@gmail.com>
2018-03-20 20:57:49 +0300

    Add name,user,password to settings.py,add details to navbar.html,add url patterns to url

--- learn/views.py
+++ learn/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+from django.http  import HttpResponse
+
+# Create your views here.
+@login_required(login_url='/accounts/login/')
+def index(request):
+    return HttpResponse('Welcome to the Online-learning')
